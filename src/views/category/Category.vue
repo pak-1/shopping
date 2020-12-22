42b30fccada374e016e3497b75a25fe52262e06e
pak-1 <1919701172@qq.com>
2020-12-22 22:16:26 +0800

    modified

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,12 +1,16 @@
 <template>
-  <div ref="Cwrapper">
-    <vue-qr
-      :text="url"
-      logoSrc="http://s3.mogucdn.com/p2/161214/103488673_65bf62g5e98f4g9b8dk1j5le13e34_140x140.png"
-      :margin="0"
-      coloDark="#F67B29"
-      colorLight="#ffffff"
-    ></vue-qr>
+  <div ref="Cwrapper" class="Cwrapper">
+    <div class="qr">
+      <vue-qr
+        :text="url"
+        logoSrc="http://s3.mogucdn.com/p2/161214/103488673_65bf62g5e98f4g9b8dk1j5le13e34_140x140.png"
+        :margin="0"
+        coloDark="#F67B29"
+        colorLight="#ffffff"
+        class="qr"
+        :size="100"
+      ></vue-qr>
+    </div>
   </div>
 </template>
 
@@ -26,4 +30,10 @@ export default {
 </script>
 
 <style scope>
+.qr {
+  margin: 10px 20px;
+}
+.qr img {
+  margin: 0;
+}
 </style>
